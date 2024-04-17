102228bcdfea80054f17e4af6763ed671e0c14fb
xiaoj <xiaojgz@163.com>
2024-04-17 15:09:16 +0800

    add config file

---
+++ config.py
@@ -0,0 +1,12 @@
+"""
+@Project :detr
+@File    :config.py
+@IDE     :PyCharm
+@Author  :xiaoj
+@Date    :2024/4/17 15:03
+"""
+coco_path = 'E:\\xiaoj\\project\\detr\\datasets\\'  # path to coco datase
+epoch = 30        # number of epochs to train
+
+
+
