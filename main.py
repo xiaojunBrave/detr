ac8c4fc6317201d909918ae2b09868d747edeae2
xiaoj <xiaojgz@163.com>
2024-04-18 10:31:03 +0800

    start_epoch 1

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def get_args_parser():
                         help='device to use for training / testing')
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--resume', default='', help='resume from checkpoint')
-    parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+    parser.add_argument('--start_epoch', default=config.start_epoch, type=int, metavar='N',
                         help='start epoch')
     parser.add_argument('--eval', action='store_true')
     parser.add_argument('--num_workers', default=2, type=int)
